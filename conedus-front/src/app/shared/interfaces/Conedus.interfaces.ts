26dc0cd5ea1efa7f520802211df0bacb542ba203
Sergio Andrés Bolaños Penagos <sergioandresbolanospenagos@gmail.com>
2023-05-02 18:41:34 -0500

    App interfaces

---
+++ conedus-front/src/app/shared/interfaces/Conedus.interfaces.ts
@@ -0,0 +1,64 @@
+export interface Colegio {
+
+}
+
+export interface Departamento {
+  departamento_id: number;
+  departamento_nombre: string;
+}
+
+export interface Municipio {
+  municipio_id: number;
+  municipio_nombre: string;
+  departamento_id: number;
+}
+
+export interface Coordenadas {
+  latitud: number,
+  longitud: number
+}
+
+export interface establecimientos {
+  establecimiento_id: number;
+  municipio_id: number;
+  establecimiento_nombre: string;
+  establecimiento_dane: string;
+  establecimiento_direccion: string;
+  establecimiento_telefono: string;
+  establecimiento_correo: string;
+  establecimiento_rector: string;
+  establecimiento_tipo: 'INSTITUCION EDUCATIVA' | 'CENTRO EDUCATIVO';
+  establecimiento_jornadas: string;
+  establecimiento_etnias: string;
+  establecimiento_sector: 'OFICIAL' | 'NO OFICIAL';
+  establecimiento_genero: 'MASCULINO' | 'FEMENINO' | 'MIXTO';
+  establecimiento_caracter: string;
+  establecimiento_licencia: 'SIN REVISAR' | 'CONDICIONAL' | 'DEFINITIVA' | 'RECONOCIMIENTO OFICIAL' | 'REVISADA ACEPTADA' | 'REVISADA NO ACEPTADA';
+  establecimiento_modelos: string;
+  establecimiento_capacidades_excepcionales: string;
+  establecimiento_discapacidades: string;
+  establecimiento_idiomas: string;
+  establecimiento_estado: 'NUEVO-ACTIVO' | 'ANTIGUO-ACTIVO' | 'CIERRE DEFINITIVO' | 'CIERRE TEMPORAL';
+  establecimiento_prestador_servicio: 'OFICIAL' | 'PERSONA NATURAL' | 'FUNDACION O CORPORACIONES' | 'COMUNIDAD RELIGIOSA' | 'SOCIEDAD' | 'EDUCACION MISIONAL CONTRATADA' | 'FEDERACIONES' | 'COOPERATIVO' | 'CAJA DE COMPENSACION' | 'CONCESION' | 'REGIMEN ESPECIAL' | 'COMUNIDAD' | 'UNIVERSIDAD';
+  establecimiento_resguardo: 'A' | 'B';
+  establecimiento_matricula_contratada: 'OFICIAL' | 'PERSONA NATURAL' | 'FUNDACION O CORPORACIONES' | 'COMUNIDAD RELIGIOSA' | 'SOCIEDAD' | 'EDUCACION MISIONAL CONTRATADA' | 'FEDERACIONES' | 'COOPERATIVO' | 'CAJA DE COMPENSACION' | 'CONCESION' | 'REGIMEN ESPECIAL' | 'COMUNIDAD' | 'UNIVERSIDAD';
+  establecimiento_internado: string;
+  establecimiento_coordenadas: Coordenadas;
+}
+
+export interface Sede {
+  sede_id: number;
+  establecimiento_id: number;
+  municipio_id: number;
+  sede_dane: string;
+  sede_nombre: string;
+  sede_zona: 'URBANA' | 'RURAL';
+  sede_direccion: string;
+  sede_telefono: string;
+  sede_email: string;
+  sede_sector: 'OFICIAL' | 'NO OFICIAL';
+  sede_estado: 'NUEVO-ACTIVO' | 'ANTIGUO-ACTIVO' | 'CIERRE DEFINITIVO' | 'CIERRE TEMPORAL';
+  sede_niveles: string;
+  sede_grados: string;
+  sede_coordenadas: Coordenadas;
+}
