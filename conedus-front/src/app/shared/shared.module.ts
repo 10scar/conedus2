41c3e64fa3ed2877d728814c501a7682de3b7784
Sergio Andrés Bolaños Penagos <sergioandresbolanospenagos@gmail.com>
2023-05-02 17:02:05 -0500

    Angular material modules added

--- conedus-front/src/app/shared/shared.module.ts
+++ conedus-front/src/app/shared/shared.module.ts
@@ -2,14 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 
-
+// Angular Material
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatInputModule,
+    MatIconModule
+  ],
+  exports: [
+    HeaderComponent,
+    MatInputModule,
+    MatIconModule
   ]
 })
 export class SharedModule { }
