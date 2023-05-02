196882cc7d1183168f6c29c6dbf75c53fac15503
Sergio Andrés Bolaños Penagos <sergioandresbolanospenagos@gmail.com>
2023-05-02 18:43:08 -0500

    List component and mdule added

--- conedus-front/src/app/modules/lists/lists.module.ts
+++ conedus-front/src/app/modules/lists/lists.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ListsComponent } from './lists.component';
+import { Router, RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: ListsComponent
+  },
+]
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListsComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
 export class ListsModule { }
