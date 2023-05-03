118bbb148a8d6e884dea0932ed50f7f3b3cbf838
Sergio Andrés Bolaños Penagos <sergioandresbolanospenagos@gmail.com>
2023-05-02 20:42:37 -0500

    Home first design

--- conedus-front/src/app/modules/home/home.component.ts
+++ conedus-front/src/app/modules/home/home.component.ts
@@ -1,10 +1,97 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Colegio, ColegioPorMunicipio, Coordenadas, Establecimiento } from 'src/app/shared/interfaces/Conedus.interfaces';
+import { ApiService } from 'src/app/shared/services/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  mejoresColombia: Establecimiento[] = [];
+  colegiosCiudad: ColegioPorMunicipio[] = [
+    {
+      municipio_nombre: 'BOGOTA',
+      establecimiento_genero: 'MIXTO',
+      establecimiento_nombre: 'Liceo Campo David',
+      establecimiento_sector: 'NO OFICIAL'
+    },
+    {
+      municipio_nombre: 'MEDELLIN',
+      establecimiento_genero: 'MIXTO',
+      establecimiento_nombre: 'Colegio San Ignacio Loyola',
+      establecimiento_sector: 'NO OFICIAL'
+    },
+    {
+      municipio_nombre: 'CALI',
+      establecimiento_genero: 'MIXTO',
+      establecimiento_nombre: 'Colegio Bilingüe Diana Oese',
+      establecimiento_sector: 'NO OFICIAL'
+    },
+    {
+      municipio_nombre: 'BARRANQUILLA',
+      establecimiento_genero: 'MIXTO',
+      establecimiento_nombre: 'IED Alexander Von Humboldt',
+      establecimiento_sector: 'OFICIAL'
+    },
+    {
+      municipio_nombre: 'CARTAGENA',
+      establecimiento_genero: 'MIXTO',
+      establecimiento_nombre: 'Aspaen Gimnasio Cartagena',
+      establecimiento_sector: 'NO OFICIAL'
+    },
+    {
+      municipio_nombre: 'SOACHA',
+      establecimiento_genero: 'MIXTO',
+      establecimiento_nombre: 'Institución Educativa La Unidad',
+      establecimiento_sector: 'NO OFICIAL'
+    }
+  ];
 
+  accesoUbicacion: boolean = false;
+
+  constructor(
+    private apiService: ApiService,
+  ){}
+
+  ngOnInit(){
+    this.requestLocation();
+  }
+
+  async requestLocation(){
+    const coordenadas = await this.getLocation();
+    console.log(coordenadas);
+  }
+
+  getLocation(): Promise<GeolocationPosition> {
+    return new Promise((resolve, reject) => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            console.log(position);
+            this.accesoUbicacion = true;
+            resolve(position);
+          },
+          (error) => {
+            this.accesoUbicacion = false;
+            console.error('El error es: '+error.message);
+            reject(error);
+          }
+        );
+      } else {
+        this.accesoUbicacion = false;
+        reject("La geolocalización no está disponible en este navegador");
+      }
+    })
+  }
+
+
+  getBestSchools(){
+    this.apiService.get()
+      .then((mejores: any) =>
+        this.mejoresColombia = mejores.mejoresColombia,
+
+      )
+      .catch()
+  }
 }
--- conedus-front/src/app/modules/home/home.module.ts
+++ conedus-front/src/app/modules/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 const routes: Routes = [
   {
@@ -16,7 +17,8 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
+    SharedModule,
     RouterModule.forChild(routes)
-  ]
+  ],
 })
 export class HomeModule { }
