b93758cadde5d56b0723c9ee9c3ff45f5fc3eb6f
mijx <124107747+mijx@users.noreply.github.com>
2023-05-06 23:48:15 -0500

    Update Main.java

--- conedus-testing/Main.java
+++ conedus-testing/Main.java
@@ -1,5 +1,15 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    /*TO DO
+    * loop1 crear lista de establecimientos
+    * loop2 crear lista de sedes, esta vez cada establecimiento tiene una sola sede
+    * loop3 agregar a cada sede el puntaje de 3 años (2019,2020,2021)
+    
+    * Revisar método InOrder de la lista circular dinámica:
+    * Exception in thread "main" java.lang.IndexOutOfBoundsException
+    * at Array.addAfter(Array.java:90)
+    * at Array.insertInOrder(Array.java:169)
+    * at Main.main(Main.java:4)    
+    
   }
 }
