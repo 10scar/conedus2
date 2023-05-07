87af38c9093495c5e5595d87f85896fa76c37ee3
10scar <gutierrezoscar200128@gmail.com>
2023-05-07 18:20:29 -0500

    top colegios

---
+++ testing/src/main/java/com/testing/TopColegios.java
@@ -0,0 +1,16 @@
+package com.testing;
+import com.testing.faker_conedus.FakerConedus;
+import com.testing.estructuras.*;
+import com.testing.models.*;
+import java.util.Scanner;
+
+
+public class TopColegios {
+   public static void menu(){
+    System.out.println("#################################### \nTop 10 Colegios ccon Datos de Prueba \n#################################### \n");     
+    System.out.println("(1).Prueba con 10 mil datos \n(2). Prueba con 100 mil datos \n(3). Prueba con 1 millón datos \n(4). Prueba con 10 millones de datos \n(5). Prueba con 100 milones datos \n");
+} 
+   public static void main(String[] args) {
+    menu();
+   }
+}
