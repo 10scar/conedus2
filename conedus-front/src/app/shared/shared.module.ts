34ddc3f4da46e2d043e2f14bbbe06cde06c27fc2
Sergio Andrés Bolaños Penagos <sergioandresbolanospenagos@gmail.com>
2023-05-22 19:57:53 -0500

    Not found page

--- conedus-front/src/app/shared/shared.module.ts
+++ conedus-front/src/app/shared/shared.module.ts
@@ -7,11 +7,13 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NotFoundComponent
   ],
   imports: [
     CommonModule,
