7e267743f2f6751ac88cfdaa91a41b38d45e1e89
fnovoas <fnovoas@unal.edu.co>
2023-05-26 17:20:21 -0500

    primitivo bst y avl 2
    .

--- testing/src/main/java/com/testing/primitivo/App.java
+++ testing/src/main/java/com/testing/primitivo/App.java
@@ -1,178 +1,172 @@
-<<<<<<< Updated upstream
 package com.testing;
-import com.testing.faker_conedus.FakerConedus;
-import com.testing.estructuras.*;
-import com.testing.models.*;
-
-
-import java.util.Scanner;
-import java.util.Random;//Reemplaza faker
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-
-
-/**
- * Hello world!
- *
- */
-public class App {
-    public static void main( String[] args ) throws FileNotFoundException
-    {
-        Random rand = new Random();//FakerConedus faker_generador =new FakerConedus();
-        ArrayCircular<Integer> arrayCircularSedes = new ArrayCircular<Integer>(10);
-        LinkedList<Integer> linkedListSedes = new LinkedList<Integer>();
-        // Doubly linked list
-
-        long start = 0L;
-        long timeCreate = 0L;
-        long timeOrder = 0L;
-        long timeRead = 0L;
-        long timeUpdate = 0L;
-        long timeDelete = 0L;
-        long timeDeleteAll = 0L;
-        
-        String tipoEstructura;
-        String ordenar;
-        int numeroDatos;
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Prueba de operaciones CRUD en estructuras con datos primitivos");
-        System.out.println("Elija tipo de estructura:\n(1) Array Circular\n(2) Lista enlazada sencilla\n(3) Lista enlazada doble");
-        tipoEstructura = sc.nextLine();
-        System.out.println();
-
-        System.out.println("¿Deseea ordenar los datos a medida que se insertan? (y/n)");
-        ordenar = sc.nextLine();
-        System.out.println();
-
-        System.out.println("¿Cuantos datos desea agregar?"); 
-        numeroDatos = Integer.parseInt(sc.nextLine());
-        System.out.println();
-
-
-        switch(tipoEstructura){
-            case "1":// Array Circular
-                // Create 
-                if (ordenar.equals("y")){
-                    start = System.nanoTime();
-                    for (int i = 0; i<numeroDatos; i++){
-                        int num = rand.nextInt(numeroDatos);
-                        arrayCircularSedes.insertInOrder(num);
-                    }
-                    timeCreate = (System.nanoTime()-start);
-                    timeOrder = 0;
-                }else{
-                    start = System.nanoTime();
-                    for (int i = 0; i<numeroDatos; i++){
-                        int num = rand.nextInt(numeroDatos);
-                        arrayCircularSedes.pushBack(num);
-                    }
-                    timeCreate = (System.nanoTime()-start);
-
-                    // Order elements
-                    start = System.nanoTime();
-                    //arrayCircularSedes.quickOrder()
-                    timeOrder = (System.nanoTime()-start);
-                }
-
-                //Read: obtener el valor del penultimo elemento en la lista
-                start = System.nanoTime();
-                arrayCircularSedes.get(arrayCircularSedes.getSize()-2);
-                timeRead = (System.nanoTime()-start);
-
-                // Delete: quitar el elemento central (peor caso porque elimina corriendo el extremo mas cerca al indice)
-                start = System.nanoTime();
-                arrayCircularSedes.get(arrayCircularSedes.getSize()/2);
-                timeDelete = (System.nanoTime()-start);
-
-                // Update: el penultimo elemento de la lista
-                start = System.nanoTime();
-                arrayCircularSedes.set(arrayCircularSedes.getSize()-1,numeroDatos + 5);
-                timeUpdate = (System.nanoTime()-start);
-
-                //Delete All
-                start = System.nanoTime();
-                while(arrayCircularSedes.getSize()>0){
-                    arrayCircularSedes.popFront();
-                }
-                timeDeleteAll = (System.nanoTime()-start);
-            break;
-
-            case "2":
-            if (ordenar.equals("y")){
-                start = System.nanoTime();
-                for (int i = 0; i<numeroDatos; i++){
-                    int num = rand.nextInt(numeroDatos);
-                    linkedListSedes.insertInOrder(num);
-                }
-                timeCreate = (System.nanoTime()-start);
-                timeOrder = 0;
-            }else{
-                start = System.nanoTime();
-                for (int i = 0; i<numeroDatos; i++){
-                    int num = rand.nextInt(numeroDatos);
-                    linkedListSedes.pushBack(num);
-                }
-                timeCreate = (System.nanoTime()-start);
-
-                // Order elements?????????????????????????????????????????????????????????????
-                start = System.nanoTime();
-                //arrayCircularSedes.quickOrder()???
-                timeOrder = (System.nanoTime()-start);
-            }
-            
-            //Read: obtener el valor del penultimo elemento en la lista
-            start = System.nanoTime();
-            linkedListSedes.get(numeroDatos-2);
-            timeRead = (System.nanoTime()-start);
-
-            // Update: cambiar el tercer elemento por 3
-            start = System.nanoTime();
-            linkedListSedes.getHead().getNext().getNext().setData(numeroDatos + 5);
-            timeUpdate = (System.nanoTime()-start);
-
-
-            // Delete: eliminar el segundo elemento
-            start = System.nanoTime();
-            try {
-                linkedListSedes.delete(rand.nextInt(numeroDatos));
-            } catch (Exception e) {
-            }
-            timeDelete = (System.nanoTime()-start);
-
-            // Delete all
-            start = System.nanoTime();
-            while(linkedListSedes.getHead()!=null){
-                linkedListSedes.popFront();
-            }
-            timeDeleteAll = (System.nanoTime()-start);
-            break;
-
-    
-            case "3":
-            break;
-        }
-
-        System.out.println("RESULTADOS");
-        System.out.printf("%-12s%-12s%-12s%-12s%-12s%-12s\n","Create","Read","Update","Delete","Order","Delete All");
-        System.out.printf("%-12d%-12d%-12d%-12d%-12d%-12d\n",timeCreate,timeRead,timeUpdate,timeDelete,timeOrder,timeDeleteAll);
-
-
-    }
-}
-
-
-
-
-
-=======
-package com.testing; //árbol BST
 
 import java.util.Random;
 
-import com.testing.estructuras.ArbolBST;
+//import com.testing.estructuras.ArbolBST;
+//*
+public class ArbolBST<T extends Comparable<T>> {
+    private Node root;
+    private int count = 0;// printTop(n)
 
+    public ArbolBST() {
+        root = null;
+    }
+
+    public void insertBST(T data) {
+        root = insert(data, root);
+    }
+
+    private Node insert(T data, Node p) {
+        if (p == null) {
+            p = new Node(data);
+        } else {
+            if (data.compareTo(p.data) < 0) {
+                p.left = insert(data, p.left);
+            } else if (data.compareTo(p.data) > 0) {
+                p.right = insert(data, p.right);
+            } else {
+                System.out.println("Item no insertado");
+            }
+        }
+        return p;
+    }
+
+    public void removeBST(T data) {
+        root = remove(data, root);
+    }
+
+    private Node remove(T data, Node p) {
+        if (p != null) {
+            if (data.compareTo(p.data) < 0) {
+                p.left = remove(data, p.left);
+            } else if (data.compareTo(p.data) > 0) {
+                p.right = remove(data, p.right);
+            } else {
+                if (p.left == null && p.right == null) {
+                    p = null;
+                } else if (p.left == null) {
+                    p = p.right;
+                } else if (p.right == null) {
+                    p = p.left;
+                } else {
+                    Node t = findMin(p.right);
+                    p.data = t.data;
+                    p.right = remove(p.data, p.right);
+                }
+            }
+        } else {
+            System.out.println("Item no está en el árbol");
+        }
+        return p;
+    }
+
+    private Node findMin(Node p) {
+        if (p != null) {
+            while (p.left != null)
+                p = p.left;
+        }
+        return p;
+    }
+
+    public void inOrderBST() {
+        System.out.print("Recorrido en inorden::");
+        if (root != null)
+            inOrder(root);
+        else
+            System.out.print(" " + "Vacio");
+        System.out.println();
+    }
+
+    public void preOrderBST() {
+        System.out.print("Recorrido en preorden:");
+        if (root != null)
+            preOrder(root);
+        else
+            System.out.print(" " + "Vacio");
+        System.out.println();
+    }
+
+    public void postOrderBST() {
+        System.out.print("Recorrido en postorden:");
+        if (root != null)
+            postOrder(root);
+        else
+            System.out.print(" " + "Vacio");
+        System.out.println();
+    }
+
+    private void inOrder(Node ptr) {
+        if (ptr.left != null)
+            inOrder(ptr.left);
+        System.out.print(" " + ptr.data);
+        if (ptr.right != null)
+            inOrder(ptr.right);
+    }
+
+    private void preOrder(Node ptr) {
+        System.out.print(" " + ptr.data);
+        if (ptr.left != null)
+            preOrder(ptr.left);
+        if (ptr.right != null)
+            preOrder(ptr.right);
+    }
+
+    private void postOrder(Node ptr) {
+        if (ptr.left != null)
+            postOrder(ptr.left);
+        if (ptr.right != null)
+            postOrder(ptr.right);
+        System.out.print(" " + ptr.data);
+    }
+
+    public void printTopBST(int n) {
+        count = 1;
+        printTop(root, n);
+    }
+
+    private Node printTop(Node ptr, int n) {// n es la cantidad de elementos que compone el top
+        if (ptr == null) {// caso base
+            return null;
+        }
+        // Buscar en el sub arbol derecho
+        Node right = printTop(ptr.right, n);
+
+        if (right != null) {
+            return right;
+        }
+        if (count > n) {
+            return ptr;// deja de buscar
+        }
+        // estan en el top
+        else if (count <= n) {
+            System.out.println(ptr.data);
+        }
+
+        count++;
+
+        // Buscar en el sub arbol izquierdo
+        return printTop(ptr.left, n);
+    }
+
+    // Inner Class: Node
+    private class Node {
+        private Node left;
+        private T data;
+        private Node right;
+
+        public Node() {
+            this(null);
+        }
+
+        public Node(T data) {
+            left = null;
+            this.data = data;
+            right = null;
+        }
+    }
+}
+
+// *
 public class Main {
     public static void main(String[] args) {
         startProcess();
@@ -210,4 +204,3 @@ public class Main {
                 "La demora insertando y luego actualizando fue de: " + (endSecondTime - startSecondTime) / 1e6 + " ms");
     }
 }
->>>>>>> Stashed changes
