9c5477698b3eef299a66814ff2247f623ec62cd9
Sergio Andrés Bolaños Penagos <sergioandresbolanospenagos@gmail.com>
2023-05-26 23:09:04 -0500

    Backend server started succesfully

---
+++ backend/src/main/java/com/conedus/backend/HolaMundoController.java
@@ -0,0 +1,14 @@
+package com.conedus.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HolaMundoController {
+    @GetMapping("/hola")
+    public String hola(){
+        return "Hola mundo";
+    }
+}
