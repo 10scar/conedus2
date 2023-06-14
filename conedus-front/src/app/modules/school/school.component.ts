9e59b2e0ee064cf2f33650bb4879e0f9e76a76b2
Sergio Andrés Bolaños Penagos <sergioandresbolanospenagos@gmail.com>
2023-06-13 23:16:27 -0500

    Added chart of ICFES info with hardcoded data

--- conedus-front/src/app/modules/school/school.component.ts
+++ conedus-front/src/app/modules/school/school.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ChartConfiguration, ChartOptions } from 'chart.js';
 import { ColegioInfo } from 'src/app/shared/interfaces/Conedus.interfaces';
 import { ApiService } from 'src/app/shared/services/api.service';
 
@@ -10,10 +11,10 @@ import { ApiService } from 'src/app/shared/services/api.service';
 })
 export class SchoolComponent {
 
-  schoolInfo!: ColegioInfo;
-  loading: boolean = true;
+  public schoolInfo!: ColegioInfo;
+  public loading: boolean = true;
 
-  imagenPorJornada: any = {
+  public imagenPorJornada: any = {
     'Mañana': 'woman.svg',
     'Tarde': 'woman.svg',
     'Noche': 'woman.svg',
@@ -21,6 +22,34 @@ export class SchoolComponent {
     'Tarde y noche': 'woman.svg',
     'Jornada completa': 'woman.svg',
   }
+
+  public lineChartData!: ChartConfiguration<'line'>['data'];
+
+  public lineChartOptions: ChartOptions<'line'> = {
+    responsive: false,
+    scales: {
+      y: {
+        type: 'linear',
+        display: true,
+        position: 'left',
+        beginAtZero: true,
+        suggestedMax: 100,
+        suggestedMin: 0,
+      },
+      y1: {
+        type: 'linear',
+        display: true,
+        position: 'right',
+        beginAtZero: true,
+        suggestedMax: 500,
+        suggestedMin: 0,
+        // grid line settings
+        grid: {
+          drawOnChartArea: false, // only want the grid lines for one axis to show up
+        },
+      },
+    }
+  };
 
   constructor(
     private route: ActivatedRoute,
@@ -45,12 +74,66 @@ export class SchoolComponent {
       establecimientoTelefono: '+57 3023420779',
       sedeZona: 'RURAL'
     }
+    this.lineChartData = {
+      labels: [2020, 2021, 2022],
+      datasets: [
+        {
+          data: [350, 320, 270],
+          label: 'Puntaje Global',
+          fill: false,
+          tension: 0.8,
+          borderColor: 'black',
+          yAxisID: 'y1'
+        },
+        {
+          data: [90, 56, 77],
+          label: 'Matemáticas',
+          fill: false,
+          tension: 0.8,
+          borderColor: 'blue',
+          yAxisID: 'y'
+        },
+        {
+          data: [45, 53, 75],
+          label: 'Sociales',
+          fill: false,
+          tension: 0.8,
+          borderColor: 'purple',
+          yAxisID: 'y'
+        },
+        {
+          data: [90, 67, 70],
+          label: 'Lectura',
+          fill: false,
+          tension: 0.8,
+          borderColor: 'red',
+          yAxisID: 'y'
+        },
+        {
+          data: [30, 45, 63],
+          label: 'Inglés',
+          fill: false,
+          tension: 0.8,
+          borderColor: 'yellow',
+          yAxisID: 'y'
+        },
+        {
+          data: [94, 56, 58],
+          label: 'Ciencias',
+          fill: false,
+          tension: 0.8,
+          borderColor: 'green',
+          yAxisID: 'y'
+        },
+      ]
+    };
     // this.apiService.get<ColegioInfo>(`sede/${id}`).then(schoolInfo => {
     //   this.loading = false;
     //   console.log(schoolInfo);
 
     // })
   }
+
 
   getJornadas(){
 
