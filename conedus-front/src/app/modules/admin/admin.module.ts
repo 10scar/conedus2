4b92212cfb424bd97d8f73a09069ea31aed34ce1
Sergio Andrés Bolaños Penagos <sergioandresbolanospenagos@gmail.com>
2023-06-21 21:46:43 -0500

    Admin module and guards

--- conedus-front/src/app/modules/admin/admin.module.ts
+++ conedus-front/src/app/modules/admin/admin.module.ts
@@ -1,12 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AdminComponent } from './admin.component';
+import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { AdminGuard } from './admin.guard';
+import { HomeComponent } from '../home/home.component';
 
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminComponent
+  },
+  {
+    path: 'home',
+    canActivate: [AdminGuard],
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  }
+]
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AdminComponent,
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    SharedModule,
+    RouterModule.forChild(routes)
+  ],
+  providers: [AdminGuard]
 })
 export class AdminModule { }
