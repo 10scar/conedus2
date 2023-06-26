38584bf84c95b4dd26dde17dc57a02f7f23a12c8
Sergio Andrés Bolaños Penagos <sergioandresbolanospenagos@gmail.com>
2023-06-26 01:51:12 -0500

    Allowed conedus as origin

--- backend/src/main/java/com/conedus/backend/BackendApplication.java
+++ backend/src/main/java/com/conedus/backend/BackendApplication.java
@@ -19,7 +19,7 @@ public class BackendApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("*")
+						.allowedOrigins("https://www.conedus.com")
 						.allowedMethods("GET", "POST", "PUT", "DELETE")
 						.allowedHeaders("*");
 			}
